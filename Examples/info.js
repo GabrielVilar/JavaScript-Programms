a99faecb5c012eabcdfed8315172bef7d8071ed8
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-02-14 15:30:46 -0300

    Info

---
+++ Examples/info.js
@@ -0,0 +1,61 @@
+//More information about the JavaScript Language
+/*
+            Comparison operators in JavaScript
+    
+    1. Equal to: "=="
+    2. Not equal to: "!="
+    3. Strict equal to: "==="
+    4. Strict not equal to: "!=="
+    5. Greater than: ">"
+    6. Less than: "<"
+    7. Greater than or equal to: ">="
+    8. Less than or equal to: "<="
+
+    More explanations:
+        The "==" operator compares two values for equality, converting 
+    their types if necessary. The "===" operator also compares 
+    two values for equality, but without any type conversion.
+        The ">" and "<" operators compare two values to determine whether 
+    one is greater than or less than the other. The ">=" and "<=" operators
+    compare two values to determine whether one is greater than or equal 
+    to or less than or equal to the other.
+        The "!=" operator checks if two values are not equal, while the 
+    "!==" operator checks if two values are not strictly equal.
+*/
+
+//Different ways of showing a message at the console in JavaScript
+/*
+    1. Using console.log() function: This is the most common way 
+    of displaying messages in the console. You can use it to display 
+    strings, variables, or objects. 
+    For example:
+*/
+console.log("Hello, world!");
+/*
+    2. Using console.warn() function: This is used to display a warning 
+    message in the console. 
+    For example:   
+*/
+console.warn("This is a warning message!");
+/*
+    3.Using console.error() function: This is used to display an error 
+    message in the console. 
+    For example:
+*/
+console.error("This is an error message!");
+/*
+    4.Using console.info() function: This is used to display an 
+    informational message in the console. 
+    For example:
+*/
+console.info("This is an informational message!");
+/*
+    5.Using console.debug() function: This is used to display a debug 
+    message in the console. 
+    For example:
+*/
+console.debug("This is a debug message!");
+/*
+    All of these functions take a message as an argument, which can be 
+    a string, variable, or object, and display it in the console.
+*/

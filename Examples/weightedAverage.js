9044d663a8a809d1883ca94a3f6e401403021f8f
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-02-14 13:40:35 -0300

    Receiving data from the prompt

--- Examples/weightedAverage.js
+++ Examples/weightedAverage.js
@@ -3,18 +3,27 @@
    with the first having a weight of 3, the second having a
    weight of 3, and the third having a weight of 4:
 */
+
+//Here we have our variables
+const prompt = require('prompt-sync')();
+var numberA = Number(prompt("Input your grades below:\nFirst grade: "));
+var numberB = Number(prompt("Second grade: "));
+var numberC = Number(prompt("Third grade: "));
+
+var result = weightedAverage(numberA, numberB, numberC);
+
 function weightedAverage(grade1, grade2, grade3){
-    return (grade1 * 3 + grade2 * 3 + grade3 * 4)/10;
+   result = (grade1 * 3 + grade2 * 3 + grade3 * 4)/10;
+   return result;
 }
 
-const result = weightedAverage(7, 10, 10);
-console.log(result);
+console.log(`The result of the weighted average is ${result}`);
 
 /*
    The function 'weightedAverage' takes in three arguments,
   witch repressnte the three grades. The function calculates
   the weighted average by multiplying each grade by its 
   corresponding weight and then dividing the sum by the
-  total weight (ten in this case). The result id then
+  total weight (ten in this case). The result is then
   logged to the console.
 */

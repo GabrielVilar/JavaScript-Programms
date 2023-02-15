2af049d0596ff7528152fe9c7882ea21906898d9
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-02-15 10:15:34 -0300

    Loops

---
+++ Examples/loops.js
@@ -0,0 +1,12 @@
+function dontTellMeLies(){
+    let counter = 0
+    while(counter <= 10){
+        console.info(`Don't tell me ${counter} or any lies`);
+        counter++;
+     }
+}
+dontTellMeLies();
+
+for(var e = 0; e <= 10; e++){
+    console.info("Ok");
+}
